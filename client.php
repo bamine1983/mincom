b860b897aafdcd1f0dc1f06131f68a8a16da4052
Amine <amine.bouchama@gmail.com>
2013-10-23 01:31:08 +0100

    Nusoap

--- client.php
+++ client.php
@@ -1,8 +1,30 @@
 <?php
 include('lib/nusoap.php');
 $serverpath ='http://www.nusoap.dev/serveur.php';
-$param = array('prenom'=>'Amine');
 $client = new nusoap_client($serverpath);
-$result = $client->call('bonjour', $param);
-print($client->request);
-?>
+$items = $client->call('getContent');
+
+if($client->fault){
+   echo "Error: <p>Code: (".$client->faultcode.")</p>";
+   echo "String: ".$client->faultstring;
+}else{
+	$r = $items;
+	$count = count($r);
+	?>
+    <table border="1">
+    <tr>
+    	<th>ID</th>
+    	<th>Titre</th>        
+    	<th>Texte</th>        
+    	<th>Date</th>        
+    </tr>
+    <?php for($i=0;$i<=$count-1;$i++){ ?>
+    <tr>
+    	<td><?php echo $r[$i]['id']; ?></td>
+    	<td><?php echo $r[$i]['title']?></td>
+    	<td><?php echo $r[$i]['intro']?></td>                
+    	<td><?php echo $r[$i]['created']?></td>        
+    </tr>
+    <?php } ?>
+    </table>
+<?php } ?>
